c523236648f053f1749ad3dc169760492868ec21
tiarap <tiaranindyap@gmail.com>
2015-03-11 14:37:24 +0700

    index.html

---
+++ index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<title>Free CSS template by ChocoTemplates.com</title>
+	<link rel="stylesheet" href="file:///E|/data/SI_semester 6/KPPL D/web/html/css/style.css" type="text/css" media="all" />
+</head>
+<body>
+<!-- Header -->
+<div id="header">
+	<div class="shell">
+		<!-- Logo + Top Nav -->
+		<div id="top">
+			<h1><a href="#">ServisIni.com</a></h1>
+			<div id="top-navigation">
+				Welcome <a href="#"><strong>Administrator</strong>
+				<span>|</span>
+				Log out</a>
+			</div>
+		</div>
+		<!-- End Logo + Top Nav -->
+		
+		<!-- Main Nav -->
+		<div id="navigation">
+			<ul>
+			    <li><a href="#" class="active"><span>Informasi</span></a></li>
+			    <li><a href="#"><span>Review</span></a></li>
+			    <li><a href="#"><span>Rekomendasi</span></a></li>
+
+			</ul>
+		</div>
+		<!-- End Main Nav -->
+	</div>
+</div>
+<!-- End Header -->
+
+<!-- Container -->
+<div id="container">
+	<div class="shell">
+		
+		<!-- Small Nav -->
+		<!-- End Small Nav -->
+		
+		<!-- Message OK -->		
+		<!-- End Message OK -->		
+		
+		<!-- Message Error -->
+		<!-- End Message Error -->
+		<br />
+		<!-- Main -->
+		<div id="main">
+			<div class="cl">&nbsp;</div>
+			
+			<!-- Content -->
+			<div id="content">
+				
+				<!-- Box -->
+				<div class="box">
+					<!-- Box Head -->
+					<div class="box-head">
+						Informasi Servis Motor
+						  <div class="right">
+							<label>search articles</label>
+							<input type="text" class="field small-field" />
+							<input type="submit" class="button" value="search" />
+						</div>
+					</div>
+					<!-- End Box Head -->	
+
+					<!-- Table -->
+					<div class="table">
+						<table width="100%" border="0" cellspacing="0" cellpadding="0">
+							<tr>
+								<th width="13"><input type="checkbox" class="checkbox" /></th>
+								<th>Title</th>
+							</tr>
+							<tr>
+								<td><input type="checkbox" class="checkbox" /></td>
+								<td><h3><a href="#">Lorem ipsum dolor sit amet, consectetur.</a></h3></td>
+								<td>&nbsp;</td>
+								<td><a href="#" class="ico del">Delete</a><a href="#" class="ico edit">Edit</a></td>
+							</tr>
+							<tr class="odd">
+								<td><input type="checkbox" class="checkbox" /></td>
+								<td><h3><a href="#">Lorem ipsum dolor sit amet, consectetur.</a></h3></td>
+								<td>&nbsp;</td>
+								<td><a href="#" class="ico del">Delete</a><a href="#" class="ico edit">Edit</a></td>
+							</tr>
+							<tr>
+								<td><input type="checkbox" class="checkbox" /></td>
+								<td><h3><a href="#">Lorem ipsum dolor sit amet, consectetur.</a></h3></td>
+								<td>&nbsp;</td>
+								<td><a href="#" class="ico del">Delete</a><a href="#" class="ico edit">Edit</a></td>
+							</tr>
+							<tr class="odd">
+								<td><input type="checkbox" class="checkbox" /></td>
+								<td><h3><a href="#">Lorem ipsum dolor sit amet, consectetur.</a></h3></td>
+								<td>&nbsp;</td>
+								<td><a href="#" class="ico del">Delete</a><a href="#" class="ico edit">Edit</a></td>
+							</tr>
+							<tr>
+								<td><input type="checkbox" class="checkbox" /></td>
+								<td><h3><a href="#">Lorem ipsum dolor sit amet, consectetur.</a></h3></td>
+								<td>&nbsp;</td>
+								<td><a href="#" class="ico del">Delete</a><a href="#" class="ico edit">Edit</a></td>
+							</tr>
+							<tr class="odd">
+								<td><input type="checkbox" class="checkbox" /></td>
+								<td><h3><a href="#">Lorem ipsum dolor sit amet, consectetur.</a></h3></td>
+								<td>&nbsp;</td>
+								<td><a href="#" class="ico del">Delete</a><a href="#" class="ico edit">Edit</a></td>
+							</tr>
+							<tr>
+								<td><input type="checkbox" class="checkbox" /></td>
+								<td><h3><a href="#">Lorem ipsum dolor sit amet, consectetur.</a></h3></td>
+								<td>&nbsp;</td>
+								<td><a href="#" class="ico del">Delete</a><a href="#" class="ico edit">Edit</a></td>
+							</tr>
+							<tr class="odd">
+								<td><input type="checkbox" class="checkbox" /></td>
+								<td><h3><a href="#">Lorem ipsum dolor sit amet, consectetur.</a></h3></td>
+								<td>&nbsp;</td>
+								<td><a href="#" class="ico del">Delete</a><a href="#" class="ico edit">Edit</a></td>
+							</tr>
+						</table>
+						
+						
+						<!-- Pagging -->
+						<div class="pagging">
+							<div class="left">Showing 1-12 of 44</div>
+							<div class="right">
+								<a href="#">Previous</a>
+								<a href="#">1</a>
+								<a href="#">2</a>
+								<a href="#">3</a>
+								<a href="#">4</a>
+								<a href="#">245</a>
+								<span>...</span>
+								<a href="#">Next</a>
+								<a href="#">View all</a>
+							</div>
+					  </div>
+						<!-- End Pagging -->
+						
+				  </div>
+					<!-- Table -->
+					
+				</div>
+				<!-- End Box -->
+				
+				<!-- Box -->
+				<div class="box">
+					<!-- Box Head -->
+					<div class="box-head">
+						<h2>Tambah Servis Motor</h2>
+					</div>
+					<!-- End Box Head -->
+					
+					<form action="" method="post">
+						
+						<!-- Form -->
+						<div class="form">
+								<p>
+								  <label>Nama Tempat Servis<span></span></label>
+									<input type="text" class="field size1" />
+						  </p>	
+								<p class="inline-field">
+									<label>Date</label>
+									<select class="field size2">
+										<option value="">23</option>
+									</select>
+									<select class="field size3">
+										<option value="">July</option>
+									</select>
+									<select class="field size3">
+										<option value="">2009</option>
+									</select>
+								</p>
+								
+								<p>
+									<span class="req">max 100 symbols</span>
+									<label>Content <span>(Required Field)</span></label>
+									<textarea class="field size1" rows="10" cols="30"></textarea>
+								</p>	
+							
+						</div>
+						<!-- End Form -->
+						
+						<!-- Form Buttons -->
+						<div class="buttons">
+							<input type="button" class="button" value="preview" />
+							<input type="submit" class="button" value="submit" />
+						</div>
+						<!-- End Form Buttons -->
+					</form>
+				</div>
+				<!-- End Box -->
+
+			</div>
+			<!-- End Content -->
+			
+			<!-- Sidebar -->
+			<div id="sidebar">
+				
+				<!-- Box -->
+				<div class="box">
+					
+					<!-- Box Head --><!-- End Box Head-->
+					
+					<div class="box-content">
+						<a href="#" class="add-button"><span>Add </span></a>
+					  <p class="select-all">&nbsp;</p>
+						<p><a href="#">Delete Selected</a></p>
+						
+						<!-- Sort -->
+						<div class="sort">
+							<label>Sort by</label>
+							<select class="field">
+								<option value="">Title</option>
+							</select>
+							<select class="field">
+								<option value="">Date</option>
+							</select>
+							<select class="field">
+								<option value="">Author</option>
+							</select>
+						</div>
+						<!-- End Sort -->
+						
+					</div>
+				</div>
+				<!-- End Box -->
+			</div>
+			<!-- End Sidebar -->
+			
+			<div class="cl">&nbsp;</div>			
+		</div>
+		<!-- Main -->
+	</div>
+</div>
+<!-- End Container -->
+
+<!-- Footer -->
+<div id="footer">
+	<div class="shell">
+	<span class="left">&copy; 2010 - CompanyName</span></div>
+</div>
+<!-- End Footer -->
+	
+</body>
+</html>
